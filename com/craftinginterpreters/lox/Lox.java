b8c979f1582dfb8bc07b70787ea170ac4cc1934b
22B71177 <cima22177@g.nihon-u.ac.jp>
2024-12-19 19:49:02 +0900

    add Parser.java

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -50,15 +50,4 @@ public class Lox {
         }
         //System.out.println(source);
     }
-
-    static void error(int line, String message) {
-        report(line, "", message);
-      }
-    
-      private static void report(int line, String where,
-                                 String message) {
-        System.err.println(
-            "[line " + line + "] Error" + where + ": " + message);
-        hadError = true;
-      }
 }
